947a9540910ac84b7c4113f3e58b121268f8c67d
NotMacaco22 <casilva@uamv.edu.ni>
2024-09-19 17:59:59 -0600

    Classes

---
+++ ExamC1/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import models.CommercialWorker;
+import models.DeliveryWorker;
+
+public class Main {
+
+    public static void main(String[] args){
+//        DeliveryWorker deliveryWorker = new DeliveryWorker("1321", "Cesar", "Silva", 19, 1000, "Casita");
+//        System.out.println(deliveryWorker.toString());
+//
+//        CommercialWorker commercialWorker = new CommercialWorker("3150", "Karen", "Fonseca", 20, 65461, 100);
+//        System.out.println(commercialWorker.toString());
+    }
+
+}
---
+++ ExamC1/src/main/java/models/OfficeWorker.java
@@ -0,0 +1,33 @@
+package models;
+
+public class OfficeWorker extends Worker {
+    private String companyPosition;
+
+    // Constructors
+    public OfficeWorker() {
+        super();
+    }
+
+    public OfficeWorker(String id, String firstName, String lastName, int age, float salary, String companyPosition) {
+        super(id, firstName, lastName, age, salary); // Super Class Constructor
+        this.companyPosition = companyPosition; // New Attribute
+    }
+
+    // Getter And Setter (For New Attribute)
+    public String getCompanyPosition() {
+        return companyPosition;
+    }
+    public void setCompanyPosition(String companyPosition) {
+        this.companyPosition = companyPosition;
+    }
+
+    // "New" To-String Function
+    @Override
+    public String toString() {
+        return "OfficeWroker{" +
+                "companyPosition='" + companyPosition + '\'' +
+                ", " + super.toString() + // Include properties from the Worker class
+                '}';
+    }
+
+}
